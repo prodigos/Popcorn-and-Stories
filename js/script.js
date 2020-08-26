7807c366cc7bf844bd2cf352a836d827e965942d
prodigos <wilghensantos@gmail.com>
2020-08-26 14:42:37 -0400

    able to get jsonreturned from api call

--- js/script.js
+++ js/script.js
@@ -1,15 +1,22 @@
-let key = "4f0354a7"
+let key = "4f0354a7";
 
 $(".submit").click(() => {
-    let userInput = $('.search').val();
+  let userInput = $(".search").val();
 
-    $.ajax({
-        url:`http://www.omdbapi.com/?apikey=${key}&${userInput}`
-    }).done((res) => {
-        $(".display").empty();
-        let movieData = res.data;
-        debugger
-        console.log(movieData);
-    })
-
-})
+  $.ajax({
+    url: `http://www.omdbapi.com/?apikey=${key}&s=${userInput}`,
+  }).done((res) => {
+      // let moviePlot = res.plot;
+      // let movieRatings = res.ratings;
+      let movieRes = res.search;
+      debugger;
+      $.each(movieRes, (i, e) => {
+          let moviePoster = e.poster;
+      $(".displayPoster").append(`<img src="${moviePoster}"/>`);
+    });
+    // $(".displayPoster").
+    // $(".display").empty();
+    // let movieData = res.data;
+    // console.log(movieData);
+  });
+});
